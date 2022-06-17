0cde5e8d173c86ce8420b255fed212bde8036aa7
Thibault Clérice <leponteineptique@gmail.com>
2022-06-17 16:36:02 +0200

    Fix with latest update of 2022-04-15.json

--- htruc/schemas/upgrade_path.py
+++ htruc/schemas/upgrade_path.py
@@ -20,10 +20,6 @@ def upgrade_2021_10_15_to_2022_04_15(catalog_record: CatalogRecord, verbose=True
     catalog_record["schema"] = "https://htr-united.github.io/schema/2022-04-15/schema.json"
     log("Upgrading from 2021-10-15 to 2022-04-15", verbose=verbose, use_log=use_log)
 
-    for author in catalog_record.get("authors", []):
-        author["type"] = "person"
-        log(f"--> Automatically upgrade author {author['name']} with `type`=`person`", verbose=verbose, use_log=use_log)
-
     catalog_record["script"] = [
         {"iso": script}
         for script in catalog_record["script"]
