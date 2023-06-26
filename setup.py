20a40d3ae5bd1c401d35e6e57200bc694ba187e0
Thibault Clérice <leponteineptique@gmail.com>
2023-06-26 11:17:33 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DESCRIPTION = 'HTRUC, a toolkit for HTR-United cataloging'
 URL = 'https://github.com/htr-united/htrvc'
 AUTHOR = 'Thibault Clérice'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = "1.0.8"
+VERSION = "1.1.0"
 
 # What packages are required for this module to be executed?
 
